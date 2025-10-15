6b3d854598d2862e50cd06c57d0ccc84043f48bf
Lorecami <clg.vivas@yavirac.edu.ec>
2025-10-15 08:45:16 -0500

    actualizacion

---
+++ src/database/database.providers.ts
@@ -0,0 +1,23 @@
+import { DataSource } from "typeorm";
+
+export const databaseProviders=[
+    {
+        provide:'DATABASE_CONNECTION',
+        useFactory:()=>{
+          const dataSource=new DataSource( {
+            type:'postgres',
+            host:'localhost',
+            port:5432,
+            username:'postgres',
+            password:'12345',
+            database:'back_end_CamilaVivas'
+
+            });
+         
+        return dataSource.initialize();
+
+        }
+    }
+
+
+]
